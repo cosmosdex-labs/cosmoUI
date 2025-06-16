b1da9f84e1f07ef7b27ad34f1966d5295308f32f
Akinbola247 <akinbolakehinde247@gmail.com>
2025-06-16 18:20:43 +0100

    contracts

---
+++ contracts/util.ts
@@ -0,0 +1,2 @@
+export const rpcUrl = process.env.PUBLIC_STELLAR_RPC_URL ?? "https://soroban-testnet.stellar.org:443"
+export const networkPassphrase = process.env.PUBLIC_STELLAR_NETWORK_PASSPHRASE ?? "Test SDF Network ; September 2015"
